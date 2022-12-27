4db76e2adcdc32e8d1ed0456fbad78719e4d8a16
Thomas Benzshawel <benzshawelt@msoe.edu>
2022-12-26 20:36:36 -0600

    Implemented functionality between Ian's and thomas's work. Created a proper way to simulate interaction with the board, created fundamental logic for moving a pawn on the board

--- boardLogic/main.py
+++ boardLogic/main.py
@@ -30,7 +30,6 @@ class ChessBoard:
 
     def __init__(self):
         # Sets up the board state as the default starting state
-        self.last_scan_status = "a1:rook;b1:knight;c1:bishop;"
 
         # These two lines are how all squares will be set up
         # a1 = digitalio.DigitalInOut(board.G1)
@@ -52,6 +51,42 @@ class ChessBoard:
             self.BLACK_KING: (7, 4)
         }
 
+        self.state1 = {
+            (1, 0): self.WHITE_PAWN,
+            (1, 1): self.WHITE_PAWN,
+            (1, 2): self.WHITE_PAWN,
+            (1, 3): self.WHITE_PAWN,
+            (1, 4): self.WHITE_PAWN,
+            (1, 5): self.WHITE_PAWN,
+            (1, 6): self.WHITE_PAWN,
+            (1, 7): self.WHITE_PAWN,
+            (0, 0): self.WHITE_ROOK,
+            (0, 7): self.WHITE_ROOK,
+            (0, 1): self.WHITE_KNIGHT,
+            (0, 6): self.WHITE_KNIGHT,
+            (0, 2): self.WHITE_BISHOP,
+            (0, 5): self.WHITE_BISHOP,
+            (0, 3): self.WHITE_QUEEN,  # If a pawn travels the length of the board, multiple queens can be on board
+            (0, 4): self.WHITE_KING,  # However, there can only be one king of each color, so no list is used
+
+            (6, 0): self.BLACK_PAWN,
+            (6, 1): self.BLACK_PAWN,
+            (6, 2): self.BLACK_PAWN,
+            (6, 3): self.BLACK_PAWN,
+            (6, 4): self.BLACK_PAWN,
+            (6, 5): self.BLACK_PAWN,
+            (6, 6): self.BLACK_PAWN,
+            (6, 7): self.BLACK_PAWN,
+            (7, 0): self.BLACK_ROOK,
+            (7, 7): self.BLACK_ROOK,
+            (7, 1): self.BLACK_KNIGHT,
+            (7, 6): self.BLACK_KNIGHT,
+            (7, 2): self.BLACK_BISHOP,
+            (7, 5): self.BLACK_BISHOP,
+            (7, 3): self.BLACK_QUEEN,
+            (7, 4): self.BLACK_KING,
+        }
+
         # set pawns
         for i in range(8):
             self.state[self.WHITE_PAWN].append((i, 1))
@@ -71,14 +106,34 @@ class ChessBoard:
             for i in range(8):
                 self.board_matrix[j][i] = True
 
-        print("Testing the matrix")
+    def print_matrix(self):
+        count = 0
+        print("Printing matrix")
         for row in self.board_matrix:
-            print(row)
+            print(count, row)
+            count += 1
 
     def check_for_pick_up(self):
         # Checks the squares that were True and sees if they are still true
         # Alternate implementation is looking at the serialized board state and looking for differences
-        pass
+        checked_dict = {}
+
+        # Checks the array to ensure there are no pieces picked up
+        for j in range(8):
+            for i in range(8):
+                if self.board_matrix[i][j]:
+                    checked_dict[(i, j)] = self.state1[(i, j)]
+
+        if len(checked_dict) != 32:
+            # get the piece not on the board
+            original_set = set(self.state1.items())
+            checked_set = set(checked_dict.items())
+            difference = original_set ^ checked_set
+            picked_up_piece = list(difference)[0]  #Provides a list where the 0th term is the cords, and the 1st is type
+            self.user_move_piece(picked_up_piece[0], picked_up_piece[1])
+            return False
+        else:
+            return True
 
     def serialize_state(self) -> bytes:
         # | 1 bit | 3 bits | 3 bits | 3 bits |
@@ -87,18 +142,28 @@ class ChessBoard:
 
     def user_move_piece(self, location, piece_type):
         # displays the correct moves the piece can make
-        print(piece_type, "can move", self.find_legal_moves(location, piece_type))
+        making_move = True
+        print(piece_type, "can move to ", self.find_legal_moves(location, piece_type))
 
         # Wait for any change in board state
+        while(making_move):
+            print("Add logic here to check and verify move")
         # If legal move, re-serialize the board state
+
         # If illegal move, alarm user and continue waiting until they make a legal move
 
-    @staticmethod
-    def find_legal_moves(location, piece_type):
+
+    def find_legal_moves(self, location, piece_type):
         legal_moves = []
         # insert logic to calc legal moves
-        print("Legal moves are:", legal_moves)
+        if piece_type == self.WHITE_PAWN:
+            print("It is a white pawn")
+            legal_moves.append((location[0] + 2, location[1]))
+            legal_moves.append((location[0] + 1, location[1]))
         return legal_moves
+
+    def find_piece_type(self, location):
+        return self.state1.get(location)
 
     def move_knight(self, loc0: tuple, loc1: tuple):
         pass
@@ -119,19 +184,20 @@ class ChessBoard:
         pass
 
 
-def sim_input():
-    answer = input("Type '1' for on, '0' for off")
+def sim_input(board_matrix):
+    i = int(input("enter desired matrix row "))
+    j = int(input("enter desired matrix column "))
+    answer = input("Type '1' for on, '0' for off ")
     if int(answer) == 1:
-        var = True
+        board_matrix[i][j] = True
     else:
-        var = False
-    return var
+        board_matrix[i][j] = False
 
 
 def run_board():
     # run = digitalio.DigitalInOut(board.D21)
     # run.direction = digitalio.Direction.INPUT
-    run = False
+
 
     run = True
 
@@ -139,7 +205,10 @@ def run_board():
     chess_board = ChessBoard()
 
     while run:
+        chess_board.print_matrix()
         chess_board.check_for_pick_up()
+        sim_input(chess_board.board_matrix)
+
 
 
 if __name__ == '__main__':
